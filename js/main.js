83f43c4e840f6467d4a46d6b2e02c938ad1cd2d6
sasha <crazy.kc.sall@gmail.com>
2017-01-31 15:56:17 +0200

    finished first slider

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let btnLeft = document.getElementById('left');
+let carousel = document.getElementsByClassName('carousel');
+let btnRight = document.getElementById('right');
+let pagination = document.querySelectorAll('.pagination>li');
+let activeImg;
+let activePag;
+let images = document.querySelectorAll('.carousel>li');
+for (let i = 0; i < images.length; i++) {
+  if (images[i].style.display == 'none') {
+    continue
+  } else {
+    activeImg = images[i];
+    activePag = pagination[i];
+  }
+}
+let nextPag = activePag.nextElementSibling;
+let nextImg = activeImg.nextElementSibling;
+let previousPag = activePag.previousElementSibling;
+let previousImg = activeImg.previousElementSibling;
+
+btnRight.onclick = () => {
+  activeImg.style.display = 'none'
+  if (nextImg) {
+    nextImg.style.display = 'block';
+    activePag.classList.remove('active');
+    nextPag.classList.add('active');
+  } else {
+    images[0].style.display = 'block';
+    activePag.classList.remove('active');
+    pagination[0].classList.add('active');
+  }
+}
+
+btnLeft.onclick = () => {
+  activeImg.style.display = 'none'
+  if (previousImg) {
+    previousImg.style.display = 'block';
+    activePag.classList.remove('active');
+    previousPag.classList.add('active');
+  } else {
+    images[images.length - 1].style.display = 'block';
+    activePag.classList.remove('active');
+    pagination[images.length - 1].classList.add('active');
+  }
+}
